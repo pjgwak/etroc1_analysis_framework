a819bf608f0c252bd089c8689cc88c9b506b82d8
Quantumapple <jongapples@gmail.com>
2022-03-25 10:31:45 -0500

    optimization and add Delta TOA w/o TWC

--- time_resolution.py
+++ time_resolution.py
@@ -9,37 +9,32 @@ parser = OptionParser()
 parser.add_option('--plotting', action='store_true', dest='plotting')
 (options, args) = parser.parse_args()
 
+def gaussianFit(delta_toa, board_number, plot_dir, twc=False):
 
-def gauss(x, a, x0, sigma):
-    return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
+    def gauss(x, a, x0, sigma):
+        return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
 
+    bins, edges = np.histogram(delta_toa.values, 100, density=False)
+    centers = edges[:-1] + np.diff(edges) / 2
+    par, _ = opt.curve_fit(gauss, centers, bins)
 
-def draw_fit_plot(board_number, centers, contents, norm, mean, sigma, ax=None):
-    if ax is not None:
-        plt.sca(ax)
-    #plt.hist(contents, bins=len(centers), range=[min(contents), max(contents)])
-    fit_y = gauss(centers, norm, mean, sigma)
-    plt.plot(centers, contents, 'o', label='data')
-    plt.plot(centers, fit_y, '-', label='fit')
-    plt.title('Board ' + str(board_number) + ': delta_ToA')
-    plt.xlabel('ToA (ns)')
-    plt.ylabel('Counts')
-    plt.legend()
-    print('Board ', board_number, ': ', 'norm: ', norm, 'mean: ', mean, ', sigma: ', sigma)
-    #plt.show()
-
-
-def draw_board(board_lst, center_lst, content_lst, norm_lst, mean_lst, sigma_lst, plot_dir):
-    bDraw = False
     px = 1/plt.rcParams['figure.dpi']  # pixel in inches
-    fig, ax = plt.subplots(1,4, constrained_layout=True, figsize=(1600*px, 300*px))
-    
-    draw_fit_plot(board_lst[0], center_lst[0], content_lst[0], norm_lst[0], mean_lst[0], sigma_lst[0], ax[0])
-    draw_fit_plot(board_lst[1], center_lst[1], content_lst[1], norm_lst[1], mean_lst[1], sigma_lst[0], ax[1])
-    draw_fit_plot(board_lst[2], center_lst[2], content_lst[2], norm_lst[2], mean_lst[2], sigma_lst[0], ax[2])
+    fig, ax = plt.subplots(constrained_layout=True, figsize=(800*px, 600*px))
+    fit_y = gauss(centers, *par)
+    ax.plot(centers, bins, 'o', label='data')
+    ax.plot(centers, fit_y, '-', label='fit: mean=%.3f, sigma=%.3f' % (par[1], par[2]))
+    ax.set_xlabel('ToA (ns)')
+    ax.set_ylabel('Counts')
+    ax.set_xlim(par[1]-0.5, par[1]+0.5)
+    ax.legend()
+    if twc:
+        ax.set_title('Board ' + str(board_number) + r': $\Delta$ ToA w/ TWC')
+        fig.savefig(plot_dir+'/gaussianFit_Board_wTWC'+str(board_number)+'.png')
+    else:
+        ax.set_title('Board ' + str(board_number) + r': $\Delta$ ToA w/o TWC')
+        fig.savefig(plot_dir+'/gaussianFit_Board_noTWC'+str(board_number)+'.png')
 
-    plt.savefig(plot_dir+'/resolution.png')
-
+    return par
 
 def main():
     with open('config.yaml') as f:
@@ -48,7 +43,7 @@ def main():
     file_name = conf['file_name']
     plot_dir = dir_path + '/' + file_name + '_plot'
     sub_file_dir = dir_path + '/' + file_name + '_sub_file'
-    
+
     b0_corr = pd.read_csv(sub_file_dir + '/' + file_name + '_b0_corr.txt', delimiter = '\s+', header=None)
     b0_corr.columns = ['toa', 'tot', 'delta_toa', 'corr_toa']
     b1_corr = pd.read_csv(sub_file_dir + '/' + file_name + '_b1_corr.txt', delimiter = '\s+', header=None)
@@ -56,68 +51,46 @@ def main():
     b3_corr = pd.read_csv(sub_file_dir + '/' + file_name + '_b3_corr.txt', delimiter = '\s+', header=None)
     b3_corr.columns = ['toa', 'tot', 'delta_toa', 'corr_toa']
     print("Read data: Done")
-    
+
     #1. Gaus delta
     #b0
     #(b1corr_toa+b3_cprr_ta)/2 - b0_corr_toa -> delta_toa for gauss fitting
     b0_corr['delta_toa_gauss'] = (b1_corr['corr_toa'] + b3_corr['corr_toa'])*0.5 - b0_corr['corr_toa']
     b1_corr['delta_toa_gauss'] = (b0_corr['corr_toa'] + b3_corr['corr_toa'])*0.5 - b1_corr['corr_toa']
     b3_corr['delta_toa_gauss'] = (b0_corr['corr_toa'] + b1_corr['corr_toa'])*0.5 - b3_corr['corr_toa']
-    #print(b0_corr)
-    #print(b1_corr)
-    
-    # Get sigmas
-    bins0, edges0 = np.histogram(b0_corr['delta_toa_gauss'].values, 100, density=False)
-    centers0 = edges0[:-1] + np.diff(edges0) / 2
-    par0, _ = opt.curve_fit(gauss, centers0, bins0)
-    norm0 = par0[0]
-    mean0 = par0[1]
-    sigma0 = par0[2]
-    
-    bins1, edges1 = np.histogram(b1_corr['delta_toa_gauss'].values, 100, density=False)
-    centers1 = edges1[:-1] + np.diff(edges1) / 2
-    par1, _ = opt.curve_fit(gauss, centers1, bins1)
-    norm1 = par1[0]
-    mean1 = par1[1]
-    sigma1 = par1[2]
 
-    bins3, edges3 = np.histogram(b3_corr['delta_toa_gauss'].values, 100, density=False)
-    centers3 = edges3[:-1] + np.diff(edges3) / 2
-    par3, _ = opt.curve_fit(gauss, centers3, bins3)
-    norm3 = par3[0]
-    mean3 = par3[1]
-    sigma3 = par3[2]
-    
-    if options.plotting:
-        draw_fit_plot(0, centers0, bins0, norm0, mean0, sigma0)
-        draw_fit_plot(1, centers1, bins1, norm1, mean1, sigma1)
-        draw_fit_plot(3, centers3, bins3, norm3, mean3, sigma3)
+    #### Get Delta TOA of each board w/o TWC ####
+    par0 = gaussianFit((b1_corr['toa'] + b3_corr['toa'])*0.5 - b0_corr['toa'], 0, plot_dir)
+    par1 = gaussianFit((b0_corr['toa'] + b3_corr['toa'])*0.5 - b1_corr['toa'], 1, plot_dir)
+    par3 = gaussianFit((b0_corr['toa'] + b1_corr['toa'])*0.5 - b3_corr['toa'], 3, plot_dir)
+    print('========== \u0394 ToA w/o TWC ==========')
+    print('b0: %.4f'%(par0[2]))
+    print('b1: %.4f'%(par1[2]))
+    print('b3: %.4f'%(par3[2]), '\n')
 
-    board_lst = [0,1,3]
-    center_lst = [centers0,centers1,centers3]
-    bin_lst = [bins0,bins1,bins3]
-    norm_lst = [norm0,norm1,norm3]
-    mean_lst = [mean0,mean1,mean3]
-    sigma_lst = [sigma0,sigma1,sigma3]
+    #### Get Delta TOA of each board w/ TWC ####
+    # [0]: Norm, [1]: Mean,  [2]: Sigma
+    par0 = gaussianFit(b0_corr['delta_toa_gauss'], 0, plot_dir, True)
+    par1 = gaussianFit(b1_corr['delta_toa_gauss'], 1, plot_dir, True)
+    par3 = gaussianFit(b3_corr['delta_toa_gauss'], 3, plot_dir, True)
+    print('========== \u0394 ToA w/ TWC ==========')
+    print('b0: %.4f'%(par0[2]))
+    print('b1: %.4f'%(par1[2]))
+    print('b3: %.4f'%(par3[2]), '\n')
 
-    draw_board(board_lst, center_lst, bin_lst, norm_lst, mean_lst, sigma_lst, plot_dir)
-    
     # Get time resoultions
     # Ex) b0 time resolution
     # a = b1_sigma^2 + b3_sigma^2 - b0_sigma^2
     # b = a * 0.5
     # b0_resolution = sqrt(b)
-    b0_res = np.sqrt((sigma1**2 + sigma3**2 - sigma0**2)*0.5)
-    b1_res = np.sqrt((sigma0**2 + sigma3**2 - sigma1**2)*0.5)
-    b3_res = np.sqrt((sigma0**2 + sigma1**2 - sigma3**2)*0.5)
-    
-    print('=== ETROC1 time resolution (ps) ===')
-    print('b0: ', b0_res*1000)
-    print('b1: ', b1_res*1000)
-    print('b3: ', b3_res*1000)
-    
+    b0_res = np.sqrt((par1[2]**2 + par3[2]**2 - par0[2]**2)*0.5)
+    b1_res = np.sqrt((par0[2]**2 + par3[2]**2 - par1[2]**2)*0.5)
+    b3_res = np.sqrt((par0[2]**2 + par1[2]**2 - par3[2]**2)*0.5)
 
+    print('========== ETROC1 time resolution ==========')
+    print('b0: %.2f ps'%(b0_res*1000))
+    print('b1: %.2f ps'%(b1_res*1000))
+    print('b3: %.2f ps'%(b3_res*1000))
 
 if __name__ == '__main__':
     main()
-
