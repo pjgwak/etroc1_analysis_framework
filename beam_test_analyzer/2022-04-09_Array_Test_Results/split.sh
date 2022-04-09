bda3c660bd3e8d420cb9d9da687e044381ccf866
pjgwak <piljun.gwak@gmail.com>
2022-04-09 07:32:11 -0500

    small update

--- beam_test_analyzer/2022-04-09_Array_Test_Results/split.sh
+++
@@ -1,5 +0,0 @@
-sed -n '1, 461866p' F9P5_F11P5_F5P5_Beam_HV235.txt > am1p18_2p18.txt
-sed -n '461867, 931823p' F9P5_F11P5_F5P5_Beam_HV235.txt > am2p18_3p18.txt
-sed -n '931824, 1392777p' F9P5_F11P5_F5P5_Beam_HV235.txt > am3p18_4p18.txt
-sed -n '1392778, 1839118p' F9P5_F11P5_F5P5_Beam_HV235.txt > am4p18_5p18.txt
-sed -n '1839119, 2503973p' F9P5_F11P5_F5P5_Beam_HV235.txt > am5p18_6p45.txt
