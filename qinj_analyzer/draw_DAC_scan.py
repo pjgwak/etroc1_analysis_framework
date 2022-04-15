8889ae99b0ea6221b631b678ddaaf3ce535cf72b
pjgwak <piljun.gwak@gmail.com>
2022-04-14 22:21:53 -0500

    update

--- qinj_analyzer/draw_DAC_scan.py
+++ qinj_analyzer/draw_DAC_scan.py
@@ -19,9 +19,8 @@ def poly1(x, a, b):
 
 def process_data(Q_value = 5):
     # Make file list -> Use config file?
-    folder_path = '2021-06-09_SinglePixel_Test_Results/Jitter_Performance_P16_QInj=1M25_internal_DAC_PreampOutOn'
-    DAC_range = [260, 360]
-    DAC_value = DAC_range[0]
+    folder_path = '2022-04-14_Array_Test_Results/Jitter_Performance_P5_QInj=1M25_external_PhaseAdj0_F27_DiscriOn_QInjEnable_PreBuffOff_DACScan'
+    DAC_value = 320 # Start value of DAC
     file_list = glob.glob(folder_path + '/*_QSel=' + str(Q_value) + '_*.dat')
     file_list.sort()
    
@@ -73,110 +72,95 @@ def process_data(Q_value = 5):
                 f.write('\t')
             f.write('\n')
 
-def draw_plots():
-    data_Q5 = pd.read_csv('q5_test.txt', delimiter = '\s+', header=None)
-    data_Q10 = pd.read_csv('q10_test.txt', delimiter = '\s+', header=None)
-    data_Q15 = pd.read_csv('q15_test.txt', delimiter = '\s+', header=None)
-    data_Q20 = pd.read_csv('q20_test.txt', delimiter = '\s+', header=None)
-    data_Q5.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
-    data_Q10.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
-    data_Q15.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
-    data_Q20.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+class Painter:
+    def __init__(self):
+        self.read_data_files()
     
-    # Draw DAC-Hits 
-    DAC_range = [data_Q5['DAC_value'].min(), data_Q5['DAC_value'].max()]
-    nBin = data_Q5['DAC_value'].max() - data_Q5['DAC_value'].min() + 1
-    #  plt.plot(data_Q5['DAC_value'], data_Q5['nHit'])
-    plt.plot(data_Q5['DAC_value'], data_Q5['nHit'], 'ro--', \
-            data_Q10['DAC_value'], data_Q10['nHit'], 'bs--', \
-            data_Q15['DAC_value'], data_Q15['nHit'], 'y^--', \
-            data_Q20['DAC_value'], data_Q20['nHit'], 'g*--')
-    plt.show()
-    plt.clf()
+    def read_data_files(self):
+        self.data_Q5 = pd.read_csv('q5_test.txt', delimiter = '\s+', header=None)
+        self.data_Q10 = pd.read_csv('q10_test.txt', delimiter = '\s+', header=None)
+        self.data_Q15 = pd.read_csv('q15_test.txt', delimiter = '\s+', header=None)
+        self.data_Q20 = pd.read_csv('q20_test.txt', delimiter = '\s+', header=None)
+        self.data_Q25 = pd.read_csv('q25_test.txt', delimiter = '\s+', header=None)
+        self.data_Q30 = pd.read_csv('q30_test.txt', delimiter = '\s+', header=None)
+        self.data_Q5.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+        self.data_Q10.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+        self.data_Q15.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+        self.data_Q20.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+        self.data_Q25.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
+        self.data_Q30.columns = ['DAC_value', 'nHit', 'TOA_code_mean', 'TOT_code_mean', 'Cal_code_mean', 'TOA_rms']
 
 
-    # Find noise peak
-    # Get inddex of maximun of nHit
-    # And get corresponding DAC_value
-    peak_Q5 = data_Q5['DAC_value'][data_Q5['nHit'].idxmax()]
-    peak_Q10 = data_Q10['DAC_value'][data_Q10['nHit'].idxmax()]
-    #  print(data_Q5['DAC_value'][data_Q5['nHit'].idxmax()])
-
-    # Find last point of signal
-    # Get maximum DAC_value which has non-zero nHit
-    last_Q5 = data_Q5['DAC_value'].loc[data_Q5['nHit'] != 0].max()
-    last_Q10 = data_Q10['DAC_value'].loc[data_Q10['nHit'] != 0].max()
-    last_Q15 = data_Q15['DAC_value'].loc[data_Q15['nHit'] != 0].max()
-    last_Q20 = data_Q20['DAC_value'].loc[data_Q20['nHit'] != 0].max()
-    last_points = np.asarray([last_Q5, last_Q10, last_Q15, last_Q20])
-    q_list = np.asarray([5, 10, 15, 20])
-    qx_temp = np.arange(0,25,1)
-    #  print(last_Q5)
+    def draw_DAC_hit(self):
+        # Draw DAC-Hits
+        DAC_range = [self.data_Q5['DAC_value'].min(), self.data_Q5['DAC_value'].max()]
+        nBin = self.data_Q5['DAC_value'].max() - self.data_Q5['DAC_value'].min() + 1
+        #  plt.plot(self.data_Q5['DAC_value'], self.data_Q5['nHit'])
+        plt.plot(self.data_Q5['DAC_value'], self.data_Q5['nHit'], 'ro--', \
+                self.data_Q10['DAC_value'], self.data_Q10['nHit'], 'bs--', \
+                self.data_Q15['DAC_value'], self.data_Q15['nHit'], 'y^--', \
+                self.data_Q20['DAC_value'], self.data_Q20['nHit'], 'g*--', \
+                self.data_Q25['DAC_value'], self.data_Q25['nHit'], 'o--',\
+                self.data_Q30['DAC_value'], self.data_Q30['nHit'], 'p--'
+                )
+        plt.show()
     
-    '''
-    # Find noise region
-    # Use 5 fC case (closest to noise level)
-    peak_idx = data_Q5['nHit'].idxmax()
-    def gauss(x, a, x0, sigma):
-        return a * np.exp(-(x - x0) ** 2 / (2 * sigma ** 2))
-    gauss_x = data_Q5['DAC_value'][peak_idx-5:peak_idx+10].values
-    gauss_x = gauss_x.astype(float)
-    gauss_y = data_Q5['nHit'][peak_idx-5:peak_idx+10].values
-    gauss_y = gauss_y.astype(float)
-    print(gauss_x)
-    print(gauss_y)
-    par, _ = opt.curve_fit(gauss, gauss_x, gauss_y, x0=peak_Q5)
-    print(par)
-    '''
+    def find_noise_region(self):
+        data = self.data_Q5.diff()
+        peak_idx = data['nHit'].nlargest(2).index.values
+        self.first_peak_DAC = self.data_Q5['DAC_value'][peak_idx[0]]
+        self.second_peak_DAC = self.data_Q5['DAC_value'][peak_idx[1]]
+        plt.plot(self.data_Q5['DAC_value'], data['nHit'], 'ro--')
+        plt.show()
 
-    # Fit and draw S curve
-    popt, _ = opt.curve_fit(poly1, q_list, last_points)
-    plt.plot(qx_temp, poly1(qx_temp, *popt), 'r--', label='fit: a=%.3f, b=%.3f' % tuple(popt))
+    def draw_S_curve(self):
+        # Find last point of signal
+        # Get maximum DAC_value which has non-zero nHit
+        last_Q5 = self.data_Q5['DAC_value'].loc[self.data_Q5['nHit'] != 0].max()
+        last_Q10 = self.data_Q10['DAC_value'].loc[self.data_Q10['nHit'] != 0].max()
+        last_Q15 = self.data_Q15['DAC_value'].loc[self.data_Q15['nHit'] != 0].max()
+        last_Q20 = self.data_Q20['DAC_value'].loc[self.data_Q20['nHit'] != 0].max()
+        last_Q25 = self.data_Q25['DAC_value'].loc[self.data_Q25['nHit'] != 0].max()
+        last_Q30 = self.data_Q30['DAC_value'].loc[self.data_Q30['nHit'] != 0].max()
+        last_points = np.asarray([last_Q5, last_Q10, last_Q15, last_Q20, last_Q25, last_Q30])
+        q_list = np.asarray([5, 10, 15, 20, 25, 30])
+        qx_temp = np.arange(0,33,1)
+        #  print(last_Q5)
 
-    plt.plot(q_list, last_points, 'bo')
-    plt.xlim(0,25)
-    plt.ylim(last_points[0]-20, last_points[-1]+20)
-    plt.fill([0,0,25,25],[peak_Q5, peak_Q10, peak_Q10, peak_Q5], color='lightgray', alpha=0.8)
-    plt.legend()
-    plt.show()
-    plt.clf()
+        # Fit and draw S curve
+        popt, _ = opt.curve_fit(poly1, q_list, last_points)
+        plt.plot(qx_temp, poly1(qx_temp, *popt), 'r--', label='fit: a=%.3f, b=%.3f' % tuple(popt))
+    
+        plt.plot(q_list, last_points, 'bo')
+        plt.xlim(0,33)
+        plt.ylim(self.first_peak_DAC-10, last_points[-1]+20)
+        plt.fill([0,0,33,33],[self.first_peak_DAC, self.second_peak_DAC, self.second_peak_DAC, self.first_peak_DAC], color='lightgray', alpha=0.8)
+        plt.legend()
+        plt.show()
+        #plt.clf()
+        
 
-
-    # Draw DAC-TOA rms
-    y_rms = data_Q5['TOA_rms'].values
-    y_rms[y_rms == 0] = np.nan
-    plt.plot(data_Q5['DAC_value'], y_rms, 'ro')
-    #  plt.show()
-    #  print(data_Q5['TOA_rms'])
-
+    def draw_DAC_TOA_rms(self):
+        '''
+        # Draw DAC-TOA rms
+        y_rms = self.data_Q5['TOA_rms'].values
+        y_rms[y_rms == 0] = np.nan
+        plt.plot(self.data_Q5['DAC_value'], y_rms, 'ro')
+        #  plt.show()
+        #  print(self.data_Q5['TOA_rms'])
+        '''
+        pass
+        
 
 def main():
-    for charge in [5, 10, 15, 20]:
+    for charge in [5, 10, 15, 20, 25, 30]:
         process_data(charge)
-    draw_plots() 
+    my_painter = Painter()
+    my_painter.draw_DAC_hit()
+    my_painter.find_noise_region()
+    my_painter.draw_S_curve()
+    
 
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-'''
-data = pd.read_csv('test.txt', delimiter = '\s+', header=None)
-data.columns = ['toa_code', 'tot_code', 'cal_code', 'hit_flag', 'toa', 'tot', 'toa_rms', 'DAC/Phase']
-print(data)
-
-#  k = data.groupby('DAC/Phase')['toa_rms'].agg(**{'mean':'mean'}).reset_index()
-k = data.groupby('DAC/Phase').count().reset_index();
-
-#  ['toa_rms'].agg(**{'mean':'mean'}).reset_index()
-print(k)
-
-
-plt.plot(k['DAC/Phase'], k['toa_code'], linestyle='--', marker='o', color='b', label='line with marker')
-plt.yscale('log')
-plt.ylim(None, None)
-plt.show()
-'''
